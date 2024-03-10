1d1da757b37ef7843043f32eec2a1ffdc504aa6f
Rahul Chauhan <56860601+rahulchauhan04@users.noreply.github.com>
2024-03-10 22:56:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-//your code here
+function setDate() {
+  const now = new Date();
+
+  const seconds = now.getSeconds();
+  const secondsDegrees = ((seconds / 60) * 360) + 90; // 90 is to offset the 90deg rotation in the CSS
+  const secondHand = document.querySelector('.second-hand');
+  secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+
+  const minutes = now.getMinutes();
+  const minutesDegrees = ((minutes / 60) * 360) + 90;
+  const minHand = document.querySelector('.min-hand');
+  minHand.style.transform = `rotate(${minutesDegrees}deg)`;
+
+  const hours = now.getHours();
+  const hoursDegrees = ((hours / 12) * 360) + 90;
+  const hourHand = document.querySelector('.hour-hand');
+  hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
+}
+
+setInterval(setDate, 1000);
+
+setDate(); // Call the function once at first to avoid delay in initial rotation
